6d22e7b95f3d609e96c46fd614af53c877cebcc7
Jacob Ormerod <jake@openreliability.org>
2014-10-31 23:22:46 -0400

    Added validation to dist argument in AbPval
    A test of ability to sync a new commitment.
    Validation of dist argument entry else "lnorm" would return weibull
    result.

--- R/AbPval.r
+++ R/AbPval.r
@@ -1,4 +1,7 @@
 AbPval <- function(fail,r2,dist="weibull")  {
+## validation of dist argument entry else "lnorm" would return weibull result
+	if(tolower(dist)!="weibull" || tolower(dist)!="lognormal")  {
+	stop("distribution not recognized - use 'weibull' or 'lognormal'")
     m <- 1
     if(tolower(dist)=="lognormal") {m <- 0}
     ret <- .Call( "CallgetPvalue", F=fail, r2, m, PACKAGE = "abremPivotals" )
