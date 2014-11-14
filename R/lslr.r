db5217cd213c454bdc99f4bc440835ef7b307b76
Jacob Ormerod <jake@openreliability.org>
2014-11-14 15:10:09 -0500

    argument validations to lslr
    NA's in arguments will cause error. Added dist argument validations and
    capability to override npar by suffix to dist.  (i.e. "weibull3p" is
    accepted)  Tests for non positive time values and ppp values >0<1.

--- R/lslr.r
+++ R/lslr.r
@@ -1,38 +1,121 @@
 lslr <- function(x, dist="weibull", npar=2, reg_method="xony")  {				
 	## a convergence limit is fixed here for 3rd parameter  convergence			
 	## no longer an argument for the R function, but still an argument to C++ functions			
-	limit<-1e-5			
-    if(is.data.frame(x)){
-        if(!is.null(x$time)){
-            if(!any(where <- (tolower(names(x)) %in% 
-                c("ppp","ppp.benard","ppp.beta","ppp.mean",
-                "ppp.hazen","ppp.km","ppp.kaplan-meier","ppp.blom")))){
-                stop(': Argument \"x\" is missing ppp columns(s)...')
-                # Todo: consider NOT supporting all ranking methods;
-            }else{
-#                if(length(x[,which(where)[1]]) < 3){
-                if(length(x[,which(where)[1]]) < 2){
-                    # no reason to not allow a perfect fit between two ppp's
-                    stop("Insufficient failure times")
-                }
-            }
-        }else{
-            stop(': Argument \"x\" is missing $time column...')
-        }
-    }
-
+	limit<-1e-5	
+	
+	if(is.vector(x))  {				
+		stop("use MRR functions for casual fitting, or pre-process with getPPP")			
+	}else{				
+		if(names(x)[1]=="time"&&names(x)[2]=="ppp")  {			
+		## will handle the output from getPPP			
+		## this format is also generated in abrem::calculateSingleFit			
+		}else{			
+			stop("dataframe argument format not recognized")		
+		}			
+	}				
+## any additional validations, such as positive numeric checking					
+## removal of potential na's, etc. could take place here					
+	if(anyNA(x))  {				
+	stop("NA not handled in lslr argument data")				
+	}				
+	if(any(c(x$time,x$ppp)<=0)) {				
+	stop("negative values or zero in lslr argument data")				
+	}				
+	if(any(c(x$ppp)>=1)) {				
+	stop("invalid percentile value in ppp")				
+	}				
+					
+## A rather extensive validation is performed on dist and npar arguments here					
+## The npar tests may be removed if we dispense with npar as an argument					
+## in the mean time it is permissible to enter dist suffixes with 2p or 3p					
+					
+	if(!any(tolower(dist) %in% 					
+		c("weibull","weibull2p","weibull3p","lognormal","lognormal2p",					
+		"lognormal3p","gumbel","gumbel2p","gumbel3p"))){					
+		stop("dist argument not recognized ")					
+	}
+	
+	if(tolower(dist)=="weibull3p" )  {					
+		dist="weibull"				
+		if(npar!=3)  {				
+			npar=3			
+			warning("npar argument overridden by dist name")			
+		}				
+	}					
+						
+	if(tolower(dist)=="weibull2p" )  {					
+		dist="weibull"				
+		if(npar!=2)  {				
+			npar=2			
+			warning("npar argument overridden by dist name")			
+		}				
+	}					
+						
+	if(tolower(dist)=="weibull" )  {
+		dist="weibull"		
+		if(npar!=2 && npar!=3)  {				
+			stop("npar not valid for distribution")			
+		}				
+	}					
+						
+	if(tolower(dist)=="lognormal3p" )  {					
+		dist="lognormal"				
+		if(npar!=3)  {				
+			npar=3			
+			warning("npar argument overridden by dist name")			
+		}				
+	}					
+						
+	if(tolower(dist)=="lognormal2p" )  {					
+		dist="lognormal"				
+		if(npar!=2)  {				
+			npar=2			
+			warning("npar argument overridden by dist name")			
+		}				
+	}					
+						
+	if(tolower(dist)=="lognormal" )  {
+		dist="lognormal"	
+		if(npar!=2 && npar!=3)  {				
+			stop("npar not valid for distribution")			
+		}				
+	}					
+						
+	if(tolower(dist)=="gumbel3p" )  {					
+		dist="gumbel"				
+		if(npar!=3)  {				
+			npar=3			
+			warning("npar argument overridden by dist name")			
+		}				
+	}					
+						
+	if(tolower(dist)=="gumbel2p" )  {					
+		dist="gumbel"				
+		if(npar!=2)  {				
+			npar=2			
+			warning("npar argument overridden by dist name")			
+		}				
+	}					
+						
+	if(tolower(dist)=="gumbel" )  {	
+		dist="gumbel"
+		if(npar!=2 && npar!=3)  {				
+			stop("npar not valid for distribution")			
+		}				
+	}					
+					
+## it is okay not to qualify casenum=0 for dist=="weibull" now					
+## no need to continue tolower testing either					
 	casenum<-0			
 	if(tolower(reg_method)=="yonx") casenum=casenum+1
 	if(npar==3) casenum=casenum+2			
 	if(dist=="lognormal") casenum=casenum+4
-	if(dist=="gumbel") casenum=casenum+8			
+	if(dist=="gumbel") casenum=casenum+8
+	
+	## return the .Call to original form			
+	resultVec<-.Call("LSLR", x$time, x$ppp, limit, casenum , package="abremPivotals")			
 
-    na.omit				
-				
-	resultVec<-.Call("LSLR", na.omit(x)$time, na.omit(x)[,which(where)[1]], limit, casenum , package="abremPivotals")	
-        # TODO: check proper usage of na.omit. Not too strict? any row with a NA value in the $time OR in the $event column will be omitted
-        # (jurgen) there was a case where lslr() failed with a mention of "object 'where' not found -> look into this
-				
+## all modifications for 0.2.10 accepted below				
 	if(casenum < 4) {			
 		if(length(resultVec)==3)  {	
 			prr<-AbPval(dim(x)[1], resultVec[3])
